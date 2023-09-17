f009d3dcd533f7c78fed4fd9ae253ba9ecb2e670
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2023-09-17 12:01:10 +0700

    Pretty more EconInfo.json
    With removing unicode escape sequence

--- UnturnedDatamining/UnturnedDatamining/EconInfoHelper.cs
+++ UnturnedDatamining/UnturnedDatamining/EconInfoHelper.cs
@@ -1,24 +1,44 @@
-﻿using System.Text.Json;
+﻿using System.Globalization;
+using System.Text.Json;
+using System.Text.RegularExpressions;
 
 namespace UnturnedDatamining;
-internal static class EconInfoHelper
+internal static partial class EconInfoHelper
 {
     public static async Task PrettyPrintEconAsync(string unturnedPath)
     {
         var econFile = Path.Combine(unturnedPath, "EconInfo.json");
-        string? json;
         await using var stream = File.Open(econFile, FileMode.Open);
 
+        string? json;
         using (var jDoc = await JsonDocument.ParseAsync(stream))
         {
             json = JsonSerializer.Serialize(jDoc, new JsonSerializerOptions { WriteIndented = true });
         }
 
-        if (!string.IsNullOrEmpty(json))
+        if (string.IsNullOrEmpty(json))
         {
-            await using var streamWriter = new StreamWriter(stream);
-            streamWriter.BaseStream.Seek(0, SeekOrigin.Begin);
-            await streamWriter.WriteAsync(json);
+            return;
         }
+
+        // replaces \u... (unicode escape sequence) to original value
+        json = UnicodeEscapeSequence().Replace(json, match =>
+        {
+            return ((char)int.Parse(match.ValueSpan[2..], NumberStyles.HexNumber)).ToString();
+        });
+
+        json = json.Replace(@"\n", "");
+
+        await using var streamWriter = new StreamWriter(stream);
+
+        // reset original content
+        stream.SetLength(0);
+        streamWriter.BaseStream.Seek(0, SeekOrigin.Begin);
+
+        await streamWriter.WriteAsync(json);
+        await streamWriter.FlushAsync();
     }
+
+    [GeneratedRegex(@"\\u([0-9A-F]{4})", RegexOptions.IgnoreCase)]
+    private static partial Regex UnicodeEscapeSequence();
 }
