f49c25c20313ac64e6552f9f37c8a258e070cf19
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2023-09-17 14:26:46 +0700

    Add `Filters.md` that formatted as markdown table

---
+++ UnturnedDatamining/UnturnedDatamining/MarkdownTableHelperEx.cs
@@ -0,0 +1,86 @@
+﻿/* This code is based of this GitHub repo:
+ * https://github.com/jpierson/to-markdown-table/blob/develop/src/ToMarkdownTable/LinqMarkdownTableExtensions.cs
+ * 
+ * MIT License
+ * 
+ * Copyright(c) 2017 Jeff
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace UnturnedDatamining;
+/// <summary>
+/// Ex: removed sort by name
+/// </summary>
+internal static class MarkdownTableHelperEx
+{
+    public static string ToMarkdownTable<T>(this IEnumerable<T> source) where T : struct
+    {
+        var properties = typeof(T).GetProperties();
+
+        var gettables
+            = properties.Select(p => new { p.Name, GetValue = (Func<object, object?>)p.GetValue, Type = p.PropertyType });
+
+        var maxColumnValues = source
+            .Select(x => gettables.Select(p => p.GetValue(x)?.ToString()?.Length ?? 0))
+            .Union(new[] { gettables.Select(p => p.Name.Length) }) // Include header in column sizes
+            .Aggregate(
+                new int[gettables.Count()].AsEnumerable(),
+                (accumulate, x) => accumulate.Zip(x, Math.Max))
+            .ToArray();
+
+        var columnNames = gettables.Select(p => p.Name);
+
+        var headerLine = "| " + string.Join(" | ", columnNames.Select((n, i) => n.PadRight(maxColumnValues[i]))) + " |";
+
+        var isNumeric = new Func<Type, bool>(type =>
+            type == typeof(Byte) ||
+            type == typeof(SByte) ||
+            type == typeof(UInt16) ||
+            type == typeof(UInt32) ||
+            type == typeof(UInt64) ||
+            type == typeof(Int16) ||
+            type == typeof(Int32) ||
+            type == typeof(Int64) ||
+            type == typeof(Decimal) ||
+            type == typeof(Double) ||
+            type == typeof(Single));
+
+        var rightAlign = new Func<Type, char>(type => isNumeric(type) ? ':' : ' ');
+
+        var headerDataDividerLine =
+            "| " +
+             string.Join(
+                 "| ",
+                 gettables.Select((g, i) => new string('-', maxColumnValues[i]) + rightAlign(g.Type))) +
+            "|";
+
+        var lines = new[]
+            {
+                    headerLine,
+                    headerDataDividerLine,
+                }.Union(
+                source
+                .Select(s =>
+                    "| " + string.Join(" | ", gettables.Select((n, i) => (n.GetValue(s)?.ToString() ?? "").PadRight(maxColumnValues[i]))) + " |"));
+
+        return lines
+            .Aggregate((p, c) => p + Environment.NewLine + c);
+    }
+}
--- UnturnedDatamining/UnturnedDatamining/Scenarios/DownloadDataFromWebsites.cs
+++ UnturnedDatamining/UnturnedDatamining/Scenarios/DownloadDataFromWebsites.cs
@@ -1,25 +1,27 @@
-﻿using System.Text;
+﻿using SDG.HostBans;
+using SDG.NetPak;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace UnturnedDatamining.Scenarios;
-internal class DownloadDataFromWebsites : IScenario
+internal partial class DownloadDataFromWebsites : IScenario
 {
-    private static readonly HashSet<(Uri uri, string fileName)> s_DataToDownload = new()
+    private static readonly HashSet<(Uri uri, string fileName, IDataFormatter dataFormatter)> s_DataToDownload = new()
     {
-        (new("https://smartlydressedgames.com/UnturnedLiveConfig.dat"), "UnturnedLiveConfig.dat"),
-        (new("https://smartlydressedgames.com/UnturnedHostBans/index.html"), "UnturnedHostBans.md"),
+        (new("https://smartlydressedgames.com/UnturnedLiveConfig.dat"), "UnturnedLiveConfig.dat", StringDataFormatter.Instance),
+        (new("https://smartlydressedgames.com/UnturnedHostBans/index.html"), "UnturnedHostBans.md", StringDataFormatter.Instance),
+        (new("https://smartlydressedgames.com/UnturnedHostBans/filters.bin"), "Filters.md", MarkDownTableDataFormatter.Instance),
     };
 
     private string m_SummaryOfDownloadedFiles = null!;
 
     public async Task<bool> StartAsync(string unturnedPath, string[] args)
     {
-        // todo: add
-        // https://smartlydressedgames.com/UnturnedHostBans/filters.bin ( fallback http://chaotic-island-paradise.s3-website-us-west-2.amazonaws.com/UnturnedHostBans/filters.bin )
-
         using var client = new HttpClient();
 
         var tasks = s_DataToDownload
-            .Select(tuple => DownloadDataAndWriteToFileAsync(client, tuple.uri, unturnedPath, tuple.fileName));
+            .Select(tuple
+                => tuple.dataFormatter.DownloadAndFormatToFileAsync(client, tuple.uri, unturnedPath, tuple.fileName));
 
         var sb = new StringBuilder();
         foreach ((string fileName, bool isFileWritten) in await Task.WhenAll(tasks))
@@ -42,29 +44,123 @@ internal class DownloadDataFromWebsites : IScenario
         return true;
     }
 
-    private static async Task<(string fileName, bool isFileWritten)> DownloadDataAndWriteToFileAsync(HttpClient client, Uri uri, string unturnedPath, string fileName)
-    {
-        var response = await client.GetStringAsync(uri);
-        var pathToFile = Path.Combine(unturnedPath, fileName);
-
-        if (!File.Exists(pathToFile))
-        {
-            await File.WriteAllTextAsync(pathToFile, response);
-            return new(fileName, true);
-        }
-
-        if (await File.ReadAllTextAsync(pathToFile) == response)
-        {
-            // content is same, ignoring
-            return new(fileName, false);
-        }
-
-        await File.WriteAllTextAsync(pathToFile, response);
-        return new(fileName, true);
-    }
-
     public async Task WriteCommitToFileAsync(string path, string fileName)
     {
         await File.WriteAllTextAsync(Path.Combine(path, fileName), $"{DateTime.UtcNow:dd MMMM yyyy} - Updated {m_SummaryOfDownloadedFiles}");
+    }
+
+    private interface IDataFormatter
+    {
+        Task<(string fileName, bool isFileWritten)> DownloadAndFormatToFileAsync(HttpClient client, Uri uri, string unturnedPath, string fileName);
+    }
+
+    private class StringDataFormatter : IDataFormatter
+    {
+        public static StringDataFormatter Instance { get; } = new();
+
+        public async Task<(string fileName, bool isFileWritten)> DownloadAndFormatToFileAsync(HttpClient client, Uri uri, string unturnedPath, string fileName)
+        {
+            var response = await client.GetStringAsync(uri);
+            var pathToFile = Path.Combine(unturnedPath, fileName);
+
+            if (!File.Exists(pathToFile))
+            {
+                await File.WriteAllTextAsync(pathToFile, response);
+                return new(fileName, true);
+            }
+
+            if (await File.ReadAllTextAsync(pathToFile) == response)
+            {
+                // content is same, ignoring
+                return new(fileName, false);
+            }
+
+            await File.WriteAllTextAsync(pathToFile, response);
+            return new(fileName, true);
+        }
+    }
+
+    private partial class MarkDownTableDataFormatter : IDataFormatter
+    {
+        public static MarkDownTableDataFormatter Instance { get; } = new();
+
+        public async Task<(string fileName, bool isFileWritten)> DownloadAndFormatToFileAsync(HttpClient client, Uri uri, string unturnedPath, string fileName)
+        {
+            var response = await client.GetByteArrayAsync(uri);
+
+            var reader = new NetPakReader();
+            reader.SetBuffer(response);
+
+            var filters = new HostBanFilters();
+            filters.ReadConfiguration(reader);
+
+            var filtersEmpty = filters.addresses.Count is 0
+                && filters.descriptionRegexes.Count is 0
+                && filters.nameRegexes.Count is 0
+                && filters.thumbnailRegexes.Count is 0
+                && filters.steamIds.Count is 0;
+
+            var pathToFile = Path.Combine(unturnedPath, fileName);
+            if (filtersEmpty && File.Exists(pathToFile))
+            {
+                // looks like new version of filters was out that we don't understand
+                return (fileName, false);
+            }
+
+            var sb = new StringBuilder();
+            sb.AppendLine("# Host bans").AppendLine();
+
+            sb.AppendLine("## IPv4 filters");
+            var addresses = filters.addresses.Select(x => new HostBanAddress(x.ToString(), x.flags));
+            sb.AppendLine(addresses.ToMarkdownTable()).AppendLine();
+
+            sb.AppendLine("## Name filters");
+            var names = filters.nameRegexes.Select(x => new HostBanRegex(x.regex.ToString(), x.flags));
+            sb.AppendLine(names.ToMarkdownTable()).AppendLine();
+
+            sb.AppendLine("## Description filters");
+            var descriptions = filters.descriptionRegexes.Select(x => new HostBanRegex(x.regex.ToString(), x.flags));
+            sb.AppendLine(descriptions.ToMarkdownTable()).AppendLine();
+
+            sb.AppendLine("## Thumbnail filters");
+            var thumbnails = filters.thumbnailRegexes.Select(x
+                => new HostBanRegexPreview(x.regex.ToString(), GetIconPreview(x), x.flags));
+            sb.AppendLine(thumbnails.ToMarkdownTable()).AppendLine();
+
+            sb.AppendLine("## SteamId filters");
+            var steamIds = filters.steamIds.Select(x => new HostBanSteamId(x.steamId.ToString(), x.flags));
+            sb.AppendLine(steamIds.ToMarkdownTable());
+
+            var value = sb.ToString();
+
+            if (await File.ReadAllTextAsync(pathToFile) == value)
+            {
+                // content is same, ignoring
+                return new(fileName, false);
+            }
+
+            await File.WriteAllTextAsync(pathToFile, value);
+            return new(fileName, true);
+        }
+
+        private static string GetIconPreview(HostBanRegexFilter filter)
+        {
+            var match = ExtractLink().Match(filter.regex.ToString());
+            if (!match.Success)
+            {
+                return string.Empty;
+            }
+
+            var link = match.Value;
+            return $"![{link}]({link})";
+        }
+
+        private record struct HostBanAddress(string Address, EHostBanFlags BanFlags);
+        private record struct HostBanSteamId(string SteamId, EHostBanFlags BanFlags);
+        private record struct HostBanRegex(string Regex, EHostBanFlags BanFlags);
+        private record struct HostBanRegexPreview(string Regex, string IconPreview, EHostBanFlags BanFlags);
+
+        [GeneratedRegex(@"\b(?:https?://)\S+\b")]
+        private static partial Regex ExtractLink();
     }
 }
