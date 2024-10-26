293de7bdb9d6a90c510741b873499c7848f7822a
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2024-10-26 17:52:13 +0700

    Fix skins deserialization trying to deserialize non json file

--- UnturnedDatamining/UnturnedDatamining/EconInfoHelper.cs
+++ UnturnedDatamining/UnturnedDatamining/EconInfoHelper.cs
@@ -1,44 +1,91 @@
-﻿using System.Globalization;
+﻿using System.Text.Encodings.Web;
 using System.Text.Json;
-using System.Text.RegularExpressions;
+using SDG.Provider;
 
 namespace UnturnedDatamining;
 internal static partial class EconInfoHelper
 {
     public static async Task PrettyPrintEconAsync(string unturnedPath)
     {
-        var econFile = Path.Combine(unturnedPath, "EconInfo.json");
-        await using var stream = File.Open(econFile, FileMode.Open);
+        var econFile = Path.Combine(unturnedPath, "EconInfo.bin");
 
-        string? json;
-        using (var jDoc = await JsonDocument.ParseAsync(stream))
+        Dictionary<int, UnturnedEconInfo> econInfos = new();
+        Dictionary<int, List<int>> bundleContents = new();
+
+        await using (var inputStream = File.Open(econFile, FileMode.Open))
         {
-            json = JsonSerializer.Serialize(jDoc, new JsonSerializerOptions { WriteIndented = true });
+            if (!ParseFile(inputStream, econInfos, bundleContents))
+            {
+                return;
+            }
         }
 
-        if (string.IsNullOrEmpty(json))
+        await using var outputStream = File.Open(Path.Combine(unturnedPath, "EconInfo.json"), FileMode.Create);
+        var options = new JsonSerializerOptions()
         {
-            return;
-        }
+            // UnturnedEconInfo uses fields
+            IncludeFields = true,
+            WriteIndented = true,
+            // unsafe <_<
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+        };
 
-        // replaces \u... (unicode escape sequence) to original value
-        json = UnicodeEscapeSequence().Replace(json, match =>
+        await JsonSerializer.SerializeAsync(outputStream, new
         {
-            return ((char)int.Parse(match.ValueSpan[2..], NumberStyles.HexNumber)).ToString();
-        });
-
-        json = json.Replace(@"\n", "");
-
-        await using var streamWriter = new StreamWriter(stream);
-
-        // reset original content
-        stream.SetLength(0);
-        streamWriter.BaseStream.Seek(0, SeekOrigin.Begin);
-
-        await streamWriter.WriteAsync(json);
-        await streamWriter.FlushAsync();
+            econInfo = econInfos,
+            bundleContent = bundleContents
+        }, options);
     }
 
-    [GeneratedRegex(@"\\u([0-9A-F]{4})", RegexOptions.IgnoreCase)]
-    private static partial Regex UnicodeEscapeSequence();
+    private static bool ParseFile(Stream stream, Dictionary<int, UnturnedEconInfo> econInfos, Dictionary<int, List<int>> bundleContents)
+    {
+        using var reader = new BinaryReader(stream);
+
+        int version = reader.ReadInt32();
+
+        if (version != 1)
+        {
+            Console.WriteLine("Version format of EconInfo.bin was changed");
+            return false;
+        }
+
+        int count = reader.ReadInt32();
+        for (var i = 0; i < count; i++)
+        {
+            var econInfo = new UnturnedEconInfo
+            {
+                name = reader.ReadString(),
+                display_type = reader.ReadString(),
+                description = reader.ReadString(),
+                name_color = reader.ReadString(),
+                itemdefid = reader.ReadInt32(),
+                marketable = reader.ReadBoolean(),
+                scraps = reader.ReadInt32(),
+                target_game_asset_guid = new Guid(reader.ReadBytes(16)),
+                item_skin = reader.ReadInt32(),
+                item_effect = reader.ReadInt32(),
+                quality = (UnturnedEconInfo.EQuality)reader.ReadInt32(),
+                econ_type = reader.ReadInt32()
+            };
+
+            econInfos.TryAdd(econInfo.itemdefid, econInfo);
+        }
+
+        count = reader.ReadInt32();
+        for (var i = 0; i < count; i++)
+        {
+            int itemDefId = reader.ReadInt32();
+            int itemCounts = reader.ReadInt32();
+
+            List<int> contents = new List<int>(itemCounts);
+            for (var j = 0; j < itemCounts; j++)
+            {
+                contents.Add(reader.ReadInt32());
+            }
+
+            bundleContents.TryAdd(itemDefId, contents);
+        }
+
+        return true;
+    }
 }
